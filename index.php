7a0c2d21353560b6b76ce0709ede7a2eef7e63b9
kamegawa <39637646+kamegawa@users.noreply.github.com>
2018-05-26 13:34:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 
+<?php
+<style type="text/css">
+	.sample2 {
+		margin:1em 0;  /* 外余白 */
+		padding:5px 5px 5px 10px;  /* 内余白 */
+		border:6px double #fff;  /* 枠線 */
+		color:#fff;  /* 文字色 */
+		background:transparent url(/content/img/bg_stripe_dark.png) repeat top left;  /* 背景 */
+		font-size:17px;  /* 文字サイズ */
+		font-weight:bold;  /* 太字 */
+	}
+</style>
+
+<div class="sample2">Content Title</div>
+?>
